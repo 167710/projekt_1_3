889676ac23367d0f46a355cec8a3e9ae99e26c40
167710 <101186945+167710@users.noreply.github.com>
2022-03-22 12:02:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Reszta z dzielenia przez 18");
+    System.out.println("Podaj liczbe: ");
+    Scanner scan = new Scanner (System.in);
+
+    int a=scan.nextInt();
+    double b=(a%10);
+    System.out.println("Reszta z dzielenia to: "+b);
+  }
+}
